8502c84049e89ea69d4a123d531e288d44d108a3
Jonathan (Log-Hei) Leung <loghei.leung@ontariotechu.net>
2022-03-24 17:13:23 -0400

    2 Scripts
    one for ui, and the other for client. not connected yet.

---
+++ Network_Midterm_Client/Assets/client.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+public class client : MonoBehaviour
+{
+
+    public GameObject myCube;
+
+    private static byte[] outBuffer = new byte[512];
+    private static IPEndPoint remoteEP;
+    private static Socket client_socket;
+
+    private float[] pos;
+    private byte[] bytepos;
+
+    private Vector3 PreviousPosition;
+
+    public static void RunClient()
+    {
+        IPAddress ip = IPAddress.Parse("127.0.0.1");//127.0.0.1//192.168.2.144");
+        remoteEP = new IPEndPoint(ip, 11111);
+
+        client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+    }
+
+    void Start()
+    {
+        myCube = GameObject.Find("Cube");
+
+        RunClient();
+
+        pos = new float[] { myCube.transform.position.x, myCube.transform.position.y, myCube.transform.position.z };
+        bytepos = new byte[pos.Length * 4];
+
+        PreviousPosition = myCube.transform.position;
+
+    }
+    void Update()
+    {
+        if (myCube.transform.position != PreviousPosition)
+        {
+            pos = new float[] { myCube.transform.position.x, myCube.transform.position.y, myCube.transform.position.z };
+
+            Buffer.BlockCopy(pos, 0, bytepos, 0, bytepos.Length);
+
+            client_socket.SendTo(bytepos, remoteEP);
+        }
+        PreviousPosition = myCube.transform.position;
+
+        if (myCube.transform.position == PreviousPosition)
+        {
+            Debug.Log("Not Sending, No Movements detected.");
+        }
+
+    }
+}
